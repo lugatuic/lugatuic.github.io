264a972a4d85cfa44e73f37936cbcee4a6a65a16
Epix <kami@epix.icu>
2023-03-27 18:04:59 -0500

    add rss generation to build script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-
+#!/bin/sh
 set -xe
 
 curl https://calendar.google.com/calendar/ical/linuxusersgroupatuic%40gmail.com/public/basic.ics |
+
 # CRLF<space> is used to fold long lines
 sed -z "s/\r\n //g" |
 
@@ -10,4 +10,7 @@ sed -z "s/\r\n //g" |
 gawk -E script.awk |
 
 # \n is better anyway
-sed 's/\r//g' > public/data.xml
+sed 's/\r//g' > public/events.xml
+
+# Generate rss feed
+xsltproc content/rss.xsl public/events.xml > public/rss.xml
